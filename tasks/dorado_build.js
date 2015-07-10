b96534fc67c1decee1033202e5c18f7ab81e67dc
alextong <alex.tong007@gmail.com>
2015-07-10 17:12:08 +0800

    删除调试信息

--- tasks/dorado_build.js
+++ tasks/dorado_build.js
@@ -9,17 +9,14 @@
 'use strict';
 
 module.exports = function (grunt) {
-
-
-
 	grunt.registerMultiTask('dorado-clean', '清除dorado项目中coffee import信息.', function () {
 		// 内部使用默认选项定义处
 		var options = this.options({
-			punctuation: "\n#上海锐道信息技术有限公司版权所有\n",
-			separator: "\n",
+			punctuation: "",
+			separator: "",
 			importBegin: '#IMPORT_BEGIN',
 			importEnd: "#IMPORT_END",
-			license: "\n#www.dorado.io\n"
+			license: ""
 		});
 		this.files.forEach(function (f) {
 			var src = f.src.filter(function (filepath) {
@@ -31,7 +28,6 @@ module.exports = function (grunt) {
 				}
 			}).map(function (filepath) {
 				var fileDate = grunt.file.read(filepath);
-				//var reg=/#IMPORT_BEGIN[\w\W\s\S]+?#IMPORT_END/i;
 				var reg = new RegExp(options.importBegin + '[\\w\\W\\s\\S]+?' + options.importEnd, "i");
 
 				fileDate = fileDate.replace(reg, "");
@@ -49,8 +45,8 @@ module.exports = function (grunt) {
 		// 内部使用默认选项定义处
 		var options = this.options({
 			punctuation: "",
-			separator: "\n",
-			license: "\n//#www.dorado.io\n"
+			separator: "",
+			license: ""
 		});
 		this.files.forEach(function (f) {
 			var src = f.src.filter(function (filepath) {
