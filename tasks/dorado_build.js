226b6803fbba85fa4e54984d73e5b439e234e3ee
alextong <alex.tong007@gmail.com>
2015-07-10 14:47:27 +0800

    新增dorado-license Task

--- tasks/dorado_build.js
+++ tasks/dorado_build.js
@@ -10,7 +10,9 @@
 
 module.exports = function (grunt) {
 
-	grunt.registerMultiTask('dorado_build', 'Dorado Ui Build.', function () {
+
+
+	grunt.registerMultiTask('dorado-clean', '清除dorado项目中coffee import信息.', function () {
 		// 内部使用默认选项定义处
 		var options = this.options({
 			punctuation: "\n#上海锐道信息技术有限公司版权所有\n",
@@ -35,6 +37,31 @@ module.exports = function (grunt) {
 				fileDate = fileDate.replace(reg, "");
 
 				return fileDate;
+			}).join(grunt.util.normalizelf(options.separator));
+			//编写版权信息和结束语
+			src = options.license + src + options.punctuation;
+			grunt.file.write(f.dest, src);
+			grunt.log.writeln('File "' + f.dest + '" created.');
+		});
+	});
+
+	grunt.registerMultiTask('dorado-license', '为dorado源文件添加版权声明.', function () {
+		// 内部使用默认选项定义处
+		var options = this.options({
+			punctuation: "",
+			separator: "\n",
+			license: "\n//#www.dorado.io\n"
+		});
+		this.files.forEach(function (f) {
+			var src = f.src.filter(function (filepath) {
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			}).map(function (filepath) {
+				return grunt.file.read(filepath);
 			}).join(grunt.util.normalizelf(options.separator));
 			//编写版权信息和结束语
 			src = options.license + src + options.punctuation;
